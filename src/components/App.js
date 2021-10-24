a371441ec7a7fa20ca4cb31333bafb8478e3644b
bthank <bthankachan@yahoo.com>
2021-10-23 21:39:25 -0400

    Nesting components changes

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+import Ninjas from './Ninjas';
+
+
+function App() {
+  return (
+    <div className="App">
+      <h1>My first React app!</h1>
+      <p>Welcome!</p>
+      <Ninjas />
+    </div>
+  );
+}
+
+export default App;
